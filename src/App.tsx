30983d0f606afce03ea9fa4bd5d2e1811b584ae5
Heber Pereira <isaiahpereira88@gmail.com>
2024-05-13 21:45:27 -0700

    basic pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import { Products } from "./types/products";
+
+import "./App.css";
+
+type ProductsProps = {
+  brand: string;
+  category: string;
+  description: string;
+  discountPercentage: number;
+  id: number;
+  images: string[];
+  price: number;
+  rating: number;
+  stock: number;
+  thumbnail: string;
+  title: string;
+};
 
 function App() {
+  const [products, setProducts] = useState<Products[]>([]);
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+  const [page, setPage] = useState<number>(1);
+
+  // fetching data from API
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        let response = await fetch("https://dummyjson.com/products?limit=100");
+        let data = await response.json();
+        if (data && data?.products) {
+          setProducts(data.products);
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.log("[ERROR FETCHING DATA]", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handlePage = (pageNumber: number) => {
+    console.log(pageNumber);
+    setPage(pageNumber);
+  };
+
+  const handlePrev = () => {
+    setPage((prev) => (prev - 1) % products.length);
+  };
+
+  const handleNext = () => {
+    setPage((prev) => (prev + 1) % products.length);
+  };
+
+  if (products.length < 1) return null;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && (
+        <div className="products">
+          {products.slice(page * 10 - 10, page * 10).map((item) => (
+            <div key={item.id} className="product">
+              <img src={item.thumbnail} alt={item.title} />
+              <p>
+                {item.title} - {item.id}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+      {!isLoading && (
+        <div className="pagination">
+          <button
+            onClick={handlePrev}
+            disabled={page === 1}
+            className={`${page === 1 ? "disabled" : ""}`}
+          >
+            Next
+          </button>
+          {[...Array(products.length / 10)].map((_, index) => (
+            <button
+              className={`page-number ${index + 1 === page ? "selected" : ""}`}
+              key={`id - ${index + 1}`}
+              onClick={() => handlePage(index + 1)}
+            >
+              {index + 1}
+            </button>
+          ))}
+          <button
+            onClick={handleNext}
+            disabled={page === 10}
+            className={`${page === 10 ? "disabled" : ""}`}
+          >
+            Prev
+          </button>
+        </div>
+      )}
     </div>
   );
 }
